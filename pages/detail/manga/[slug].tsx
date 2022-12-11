8388b891008b562158a99a84d8aeaf602db3ba8a
hanan.rivai <hanan.rivai@JKT-MAC-2246.local>
2022-12-12 03:07:19 +0700

    remmove detail content

--- pages/detail/manga/[slug].tsx
+++ pages/detail/manga/[slug].tsx
@@ -1,10 +1,8 @@
 import Layout from "../../../base/layout";
-import { Content } from "../../../dummy/content";
 
 export default function Home() {
   return (
     <Layout>
-      <Content />
     </Layout>
   );
 }
